b2c41e2b0ec88c4d7be424a507b8d0e1fc9543ff
Priyansh <91priyansh@gmail.com>
2021-09-11 11:45:13 +0530

    first commit

---
+++ lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xffF98367);
+const Color secondaryColor = Color(0xff434B5D); //work as secondary color
+const Color backgroundColor = Colors.white;
+const Color surfaceColor = Color(0xffFFEEEA);
+const Color canvasColor = Color(0xffFFF4DE);
